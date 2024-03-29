a2f3cadffae269de805362a603bcf57e4327e12e
Jaime <jaimeperezperez9012@gmail.com>
2024-03-29 09:35:19 -0600

    comentario

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import "./App.css";
 function App() {
   return (
     <>
-      <h1 className="text-red-500 text-2xl">Crud React</h1>
+      <h1 className="text-red-500 text-2xl">Listado de Roles</h1>
     </>
   );
 }
