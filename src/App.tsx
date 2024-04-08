46984d19868fb68348059376b115e33449df63be
Jaime <jaimeperezperez9012@gmail.com>
2024-04-08 13:48:34 -0600

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import "./App.css";
-
 function App() {
   return (
     <>
-      <h1 className="text-red-500 text-2xl">Listado de Roles</h1>
+      <h1 className=" text-2xl text-red-300">Listado de Roles</h1>
     </>
   );
 }
 
 export default App;
+
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,10 @@
 /** @type {import('tailwindcss').Config} */
+const {nextui} = require("@nextui-org/react");
 export default {
-  content: ["./src/**/*.{html,js,tsx}"],
+  content: ["./src/**/*.{html,js,tsx}","./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}", './node_modules/@nextui-org/theme/dist/components/(button|snippet|code|input).js'],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [nextui()]
 }
 
